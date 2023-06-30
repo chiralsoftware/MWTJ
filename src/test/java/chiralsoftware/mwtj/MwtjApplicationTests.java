fa7762dabcbe997e8ac3a74ca68025797c234c73
Eric <eric@chiralsoftware.com>
2023-06-30 13:40:26 -0700

    updated guava, added some testing

--- src/test/java/chiralsoftware/mwtj/MwtjApplicationTests.java
+++ src/test/java/chiralsoftware/mwtj/MwtjApplicationTests.java
@@ -1,13 +1,43 @@
 package chiralsoftware.mwtj;
 
+import chiralsoftware.mwtj.controllers.FileController;
+import chiralsoftware.mwtj.controllers.IndexController;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class MwtjApplicationTests {
+    
+    @Value("${local.server.port}")
+    private int port;
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private IndexController indexController;
+    
+    @Autowired
+    private FileController fileController;
+    
+    private Object testing = null;
+    
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void contextLoads() {
+        assertThat(indexController).isNotNull();
+        assertThat(fileController).isNotNull();
+        assertThat(restTemplate.getForObject("http://localhost:" + port + "/", String.class)).contains("One hour file sharing");
+//        assertThat(testing).isNotNull();
+    }
+    
+    @Test
+    public void index() throws Exception {
+        
+    }
 
 }
